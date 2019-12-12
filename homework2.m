113b6ed44d0795c9bee2a5a07a14a922b2b6ceac
Mbonea-Mjema <mjema86@gmail.com>
2019-12-12 22:00:53 +0200

    First trial

---
+++ homework2.m
@@ -0,0 +1,53 @@
+% open the files
+
+base_dir = '.\\Frames Folder\\Frames Folder\\Frame%d.jpg';
+no_frames =15;
+
+for n=1:no_frames
+  images{n} = rgb2gray(imread(sprintf(base_dir,n)));
+end
+
+
+[h,w] =size(images{1});
+Image_sum=double(zeros(h,w));
+
+
+for n=1:no_frames
+  Image_sum = Image_sum+double(images{n});
+end
+
+
+Img_mean =uint8(Image_sum/no_frames);
+figure(1);
+imshow(Img_mean);
+
+tem=Img_mean;
+
+%% standard Deviation
+Image_sum=double(zeros(h,w));
+for n=1:no_frames
+            
+image_sample= double(images{n});
+mean_value= double(Img_mean);
+Image_sum=Image_sum+(image_sample-mean_value).^2;
+
+
+end
+
+
+stdv_image=int8(sqrt(Image_sum/(no_frames-1)));
+figure(2);
+imshow(stdv_image);
+
+
+%% Question 2
+
+image_sample =double(images{10});
+Img_mean=double(Img_mean);
+stdv_image=double(stdv_image);
+mask = double(normpdf(image_sample,Img_mean,stdv_image)>0);
+figure(3);
+imshow(int8(image_sample.*mask))
+
+
+
